700545eaef089fa58c7371c77c24a4cff8f522e2
ColmSam2319 <colmsammon13@gmail.com>
2022-07-26 19:49:28 +0000

    corrected bad java link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
+console.log("Connected")
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
